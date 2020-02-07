605e7a168ea2263770b0504a15f61055e01093d3
ldex <laurent.duveau+git@gmail.com>
2020-02-06 20:57:50 -0500

    before Angular 9

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,13 +1,14 @@
 import { Observable, Subscription } from 'rxjs';
 import { ProductService, FavouriteService, CartService, SeoService, NotificationService } from './../../services';
 import { Product } from './../product.interface';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.css']
+  styleUrls: ['./product-detail.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProductDetailComponent implements OnInit {
 
