d9066163baff973f1634227e5e238c43639f2511
ldex <laurent.duveau+git@gmail.com>
2021-09-15 15:06:04 -0400

    improved orderBy pipe (thanks Pascal Ouellet!)

--- src/app/products/orderBy.pipe.ts
+++ src/app/products/orderBy.pipe.ts
@@ -4,7 +4,7 @@ import {Pipe, PipeTransform} from '@angular/core';
 export class OrderBy implements PipeTransform {
 
   static _orderByComparator(a:any, b:any):number{
-    
+
     if((isNaN(parseFloat(a)) || !isFinite(a)) || (isNaN(parseFloat(b)) || !isFinite(b))){
       //Isn't a number so lowercase the string to properly compare
       if(a.toLowerCase() < b.toLowerCase()) return -1;
@@ -15,18 +15,19 @@ export class OrderBy implements PipeTransform {
       if(parseFloat(a) < parseFloat(b)) return -1;
       if(parseFloat(a) > parseFloat(b)) return 1;
     }
-    
+
     return 0; //equal each other
   }
 
-  transform(input:any, [config = '+']): any{
-        
+  transform<T>(values: ReadonlyArray<T>, [config = '+']): Array<T>{
+    const input = values.concat();
+
     if(!Array.isArray(input)) return input;
 
     if(!Array.isArray(config) || (Array.isArray(config) && config.length == 1)){
       var propertyToCheck:string = !Array.isArray(config) ? config : config[0];
       var desc = propertyToCheck.substr(0, 1) == '-';
-            
+
        //Basic array
        if(!propertyToCheck || propertyToCheck == '-' || propertyToCheck == '+'){
          return !desc ? input.sort() : input.sort().reverse();
@@ -53,7 +54,7 @@ export class OrderBy implements PipeTransform {
 
             var comparison = !desc ? OrderBy._orderByComparator(a[property], b[property])
                 : -OrderBy._orderByComparator(a[property], b[property]);
-                    
+
             //Don't return 0 yet in case of needing to sort by next property
             if(comparison != 0) return comparison;
           }
