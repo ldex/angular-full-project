73e211841d873fbf40af4753d2a52cceb411efd3
ldex <laurent.duveau+git@gmail.com>
2022-01-23 12:00:27 -0500

    fix offline mode

--- src/app/errors/global-error-handler.ts
+++ src/app/errors/global-error-handler.ts
@@ -1,26 +1,20 @@
 import { ErrorHandler, Injectable } from "@angular/core";
-import { ErrorService } from '../services';
-import { ErrorDialogService } from '../services/error-dialog.service';
+import { ErrorService } from "../services";
+import { ErrorDialogService } from "../services/error-dialog.service";
 
 @Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
   constructor(
     private errorDialogService: ErrorDialogService,
     private errorService: ErrorService
-  ) { }
+  ) {}
 
   handleError(error: Error) {
-    if (!navigator.onLine) {
-      // Handle offline error
-      this.errorDialogService.openDialog('No Internet Connection');
-    } else {
-      // Handle Client Error (Angular Error, ReferenceError...)
-      this.errorService.log(error);
-      this.errorDialogService.openDialog(
-        error?.message || "Undefined client error"
-      );
-      console.error("Error from global error handler", error);
-    }
-
+    // Handle Client Error (Angular Error, ReferenceError...)
+    this.errorService.log(error);
+    this.errorDialogService.openDialog(
+      error?.message || "Undefined client error"
+    );
+    console.error("Error from global error handler", error);
   }
 }
