2ac092912f65c29cd8bb48117a09ae2d891301f1
ldex <laurent.duveau+git@gmail.com>
2022-06-02 21:22:30 -0400

    Update for strongly typed form

--- src/app/products/product-insert/product-insert.component.ts
+++ src/app/products/product-insert/product-insert.component.ts
@@ -5,11 +5,22 @@ import {
   FormBuilder,
   FormGroup,
   FormControl,
-  Validators
+  Validators,
+  NonNullableFormBuilder
 } from '@angular/forms';
 import { NotificationService, DialogService } from '../../services';
 import { Observable, from } from 'rxjs';
 import { CustomValidators } from '../../customValidators';
+import { Product } from '../product.interface';
+
+interface ProductForm {
+  name: FormControl<string>;
+  price: FormControl<number>;
+  description: FormControl<string>;
+  imageUrl: FormControl<string>;
+  discontinued: FormControl<boolean>;
+  fixedPrice: FormControl<boolean>;
+}
 
 @Component({
   selector: 'app-product-insert',
@@ -18,22 +29,24 @@ import { CustomValidators } from '../../customValidators';
 })
 export class ProductInsertComponent implements CanDeactivate<any>, OnInit {
 
-  insertForm: FormGroup;
-  name: FormControl;
-  price: FormControl;
-  description: FormControl;
-  imageUrl: FormControl;
+  insertForm: FormGroup<ProductForm>;
+  name: FormControl<string>;
+  price: FormControl<number>;
+  description: FormControl<string>;
+  imageUrl: FormControl<string>;
+
   submitted: boolean = false;
 
   constructor(
-    private fb: FormBuilder,
+    private fb: NonNullableFormBuilder,
     private productService: ProductService,
     private notificationService: NotificationService,
     private router: Router,
     private dialogService: DialogService) { }
 
   onSubmit() {
-    let newProduct = this.insertForm.value;
+    let newProduct: Product = this.insertForm.value as Product;
+
     this.submitted = true;
     this.productService
       .insertProduct(newProduct)
@@ -50,20 +63,20 @@ export class ProductInsertComponent implements CanDeactivate<any>, OnInit {
   ngOnInit() {
     let validImgUrlRegex: string = '^(https?\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,5}(?:\/\S*)?(?:[-A-Za-z0-9+&@#/%?=~_|!:,.;])+\.(?:jpg|jpeg|gif|png))$';
 
-    this.name = new FormControl('', [Validators.required, Validators.maxLength(50), CustomValidators.productNameValidator]);
-    this.price = new FormControl('', [Validators.required, Validators.min(0), Validators.max(10000000)]);
-    this.description = new FormControl('', [Validators.minLength(3), Validators.maxLength(50)]);
-    this.imageUrl = new FormControl('', [Validators.pattern(validImgUrlRegex)]);
+    this.name = new FormControl<string>('', [Validators.required, Validators.maxLength(50), CustomValidators.productNameValidator]);
+    this.price = new FormControl<number>(null, [Validators.required, Validators.min(0), Validators.max(10000000)]);
+    this.description = new FormControl<string>('', [Validators.minLength(3), Validators.maxLength(50)]);
+    this.imageUrl = new FormControl<string>('', [Validators.pattern(validImgUrlRegex)]);
 
     this.insertForm = this.fb.group(
       {
-        'name': this.name,
-        'price': this.price,
-        'description': this.description,
-        'discontinued': false,
-        'fixedPrice': false,
-        'imageUrl': this.imageUrl
-      }, { validator: CustomValidators.priceWithDescription }
+        name: this.name,
+        price: this.price,
+        description: this.description,
+        imageUrl: this.imageUrl,
+        discontinued: new FormControl(false),
+        fixedPrice: new FormControl(false)
+      }, { validators: CustomValidators.priceWithDescription }
     );
   }
 
